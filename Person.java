52cf876a75aec87f674f721cb6ccb973e236b654
akshay-1231 <akshayprajapati424206@gmail.com>
2024-10-02 19:11:34 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,28 @@
+package assignment2;
+
+public final class Person {
+	private final String name;
+	private final int age;
+
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+     //name and age can not changed
+	
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+
+
+}
---
+++ TestPerson.java
@@ -0,0 +1,16 @@
+package assignment2;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Person person=new Person("akshay",25);
+       System.out.println(person);
+    
+       System.out.println(person.getName());
+       System.out.println(person.getAge());
+     
+       
+	}
+
+}
